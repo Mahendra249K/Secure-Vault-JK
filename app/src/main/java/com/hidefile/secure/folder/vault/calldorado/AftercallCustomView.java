2ab4eccbed86d560e28bea939272c757bdd27bdc
Mahendra249K <mahendrakatariya249@gmail.com>
2025-01-24 11:51:24 +0530

    push add after call screen layouts

--- app/src/main/java/com/hidefile/secure/folder/vault/calldorado/AftercallCustomView.java
+++ app/src/main/java/com/hidefile/secure/folder/vault/calldorado/AftercallCustomView.java
@@ -1,5 +1,7 @@
 package com.hidefile.secure.folder.vault.calldorado;
 
+import static android.view.View.inflate;
+
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
@@ -8,8 +10,7 @@ import com.calldorado.ui.aftercall.CallerIdActivity;
 import com.calldorado.ui.views.custom.CalldoradoCustomView;
 import com.hidefile.secure.folder.vault.AdActivity.MyApplication;
 import com.hidefile.secure.folder.vault.R;
-
-import static android.view.View.inflate;
+import com.hidefile.secure.folder.vault.dashex.Str11;
 
 
 public class AftercallCustomView extends CalldoradoCustomView {
@@ -34,7 +35,7 @@ public class AftercallCustomView extends CalldoradoCustomView {
                 String linkMokalvi = "\nHey,Check out this " + context.getString(R.string.app_setting_name) + "\n\n";
                 linkMokalvi = linkMokalvi + "https://play.google.com/store/apps/details?id=" + MyApplication.Companion.getAPPLICATION_ID() + "\n\n";
                 shareIntent.putExtra(Intent.EXTRA_TEXT, linkMokalvi);
-                ((CallerIdActivity)getCalldoradoContext()).startActivity(Intent.createChooser(shareIntent, "choose one"));
+                getCalldoradoContext().startActivity(Intent.createChooser(shareIntent, "choose one"));
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -42,15 +43,50 @@ public class AftercallCustomView extends CalldoradoCustomView {
         });
 
 
-//        view.findViewById(R.id.MobileFalashlightOnOffLayout).setOnClickListener(view -> {
-//            try {
-//                Intent i = new Intent(getCalldoradoContext(), IncomingKall_PhleshLightActivity.class);
-//                getCalldoradoContext().startActivity(i);
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        });
+        view.findViewById(R.id.llPhoto).setOnClickListener(view -> {
+            try {
+                Intent i = new Intent(getCalldoradoContext(), Str11.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                getCalldoradoContext().startActivity(i);
+
+                if (getCalldoradoContext() instanceof CallerIdActivity) {
+                    ((CallerIdActivity) getCalldoradoContext()).finish();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        view.findViewById(R.id.llVideo).setOnClickListener(view -> {
+            try {
+                Intent i = new Intent(getCalldoradoContext(), Str11.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                getCalldoradoContext().startActivity(i);
+
+                if (getCalldoradoContext() instanceof CallerIdActivity) {
+                    ((CallerIdActivity) getCalldoradoContext()).finish();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        view.findViewById(R.id.llFiles).setOnClickListener(view -> {
+            try {
+                Intent i = new Intent(getCalldoradoContext(), Str11.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                getCalldoradoContext().startActivity(i);
+
+                if (getCalldoradoContext() instanceof CallerIdActivity) {
+                    ((CallerIdActivity) getCalldoradoContext()).finish();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
         return view;
     }
